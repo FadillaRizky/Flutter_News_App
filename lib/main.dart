3f51eac67d2acc97d3e9d0c1e3d53cbca9b9e2c8
Enricko <enricko.putra028@gmail.com>
2023-12-18 12:50:17 +0700

    Push Firebase Integration Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_news_app/firebase_options.dart';
 import 'package:flutter_news_app/login.dart';
-import 'package:flutter_news_app/news_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -14,10 +22,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      builder: EasyLoading.init(),
       title: 'Flutter Demo',
-      theme: ThemeData(
-          fontFamily: 'Poppins'
-      ),
+      theme: ThemeData(fontFamily: 'Poppins'),
       home: LoginPage(),
     );
   }
